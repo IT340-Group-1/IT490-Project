d9f88e36947ecc8616b61f8266df55e06df8866a
Morgan Hah <mjh58@njit.edu>
2023-02-01 16:34:23 -0500

    added python and pika installation in composer script

--- Messaging/compose.sh
+++ Messaging/compose.sh
@@ -1,3 +1,5 @@
-sudo apt install python3-pip
+sudo apt-get update
+sudo apt-get install rabbitmq-server
 sudo service rabbitmq-server start
-pip install -r requirements.txt
+sudo apt-get install python3 python3-dev
+sudo pip3 install pika
