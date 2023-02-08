7a18efbe54ceffca98c7717ebe8f676194faa226
Morgan Hah <mjh58@njit.edu>
2023-02-07 19:06:01 -0500

    Compose file installs erlang and rabbitmq. run file starts rabbitmq

--- Messaging/compose.sh
+++ Messaging/compose.sh
@@ -1,5 +1,30 @@
-sudo apt-get update
-sudo apt-get install rabbitmq-server
-sudo service rabbitmq-server start
-sudo apt-get install python3 python3-dev
-sudo pip3 install pika
+sudo apt-get install apt-transport-https
+# Import Erlang GPG Key
+sudo apt update
+sudo apt install curl software-properties-common apt-transport-https lsb-release
+curl -fsSL https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/erlang.gp
+
+# Add Erlang Repository to Ubuntu
+sudo apt update
+sudo apt install erlang
+
+# Install Erlang on Ubuntu
+sudo apt update
+sudo apt install erlang
+
+## Team RabbitMQ's main signing key
+curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/com.rabbitmq.team.gpg > /dev/null
+
+## Launchpad PPA that provides modern Erlang releases
+curl -1sLf "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xf77f1eda57ebb1cc" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg > /dev/null
+
+## PackageCloud RabbitMQ repository
+curl -1sLf "https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/io.packagecloud.rabbitmq.gpg > /dev/null
+
+sudo echo "deb [signed-by=/usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg] http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu bionic main
+deb-src [signed-by=/usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg] http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+
+sudo echo "deb [signed-by=/usr/share/keyrings/io.packagecloud.rabbitmq.gpg] https://packagecloud.io/rabbitmq/rabbitmq-server/ubuntu/ bionic main
+deb-src [signed-by=/usr/share/keyrings/io.packagecloud.rabbitmq.gpg] https://packagecloud.io/rabbitmq/rabbitmq-server/ubuntu/ bionic main" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+
+# MUST RUN "sudo nano /etc/apt/sources.list" ONCE IN THE FILE, UNCOMMENT ANY LINE THAT STARTS WITH 'deb' THEN SAVE AND EXIT EDITOR
