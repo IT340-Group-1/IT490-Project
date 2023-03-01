67db039eb0d57ac50ee6c4bc736d9f8447f327bd
eilatanatalie <ned8@njit.edu>
2023-03-01 17:15:19 -0500

    tweaking configuration of files

--- service_composer/service_composer.sh
+++ service_composer/service_composer.sh
@@ -1,15 +1,15 @@
-ssh -i "cra_ssh_key.pem" ubuntu@$(<../backend/ip.txt) "bash -s" <<"ENDSSH"
-  cd ~/IT490-Project/backend
+ssh -i ~/.ssh/cra_ssh_key.pem ubuntu@$(<../backend/ip.txt) "bash -s" <<"ENDSSH"
+  cd ~/Project/IT490-Project/backend
   nohup python3 -m bermq > bermq.out 2>&1 </dev/null &
 ENDSSH
 
-ssh -i "cra_ssh_key.pem" ubuntu@$(<../database/ip.txt) "bash -s" <<"ENDSSH"
-  sudo systemctl start mysql.service
+ssh -i ~/.ssh/cra_ssh_key.pem ubuntu@$(<../database/ip.txt) "bash -s" <<"ENDSSH"
+  sudo service mysql start
   cd ~/IT490-Project/database
   nohup python3 dbrmq.py > dbrmq.out 2>&1 </dev/null &
 ENDSSH
 
-ssh -i "cra_ssh_key.pem" ubuntu@$(<../frontend/ip.txt) "bash -s" <<"ENDSSH"
-  cd ~/IT490-Project/frontend
+ssh -i ~/.ssh/cra_ssh_key.pem ubuntu@$(<../frontend/ip.txt) "bash -s" <<"ENDSSH"
+  cd ~/Project/IT490-Project/frontend
   nohup /home/ubuntu/.local/bin/gunicorn --workers 4 --bind 0.0.0.0:7007 'craui:create_app()' > craui.out 2>&1 </dev/null &
 ENDSSH
