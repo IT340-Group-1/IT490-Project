d4e5cce4ec477414ddd4db1cd7fc2240a24498c4
morganhah99 <mjh58@njit.edu>
2023-04-06 16:46:44 -0400

    Revert "endpoints"
    This reverts commit ad853b1b3d57ea80ce9ddf01b3eecf693dc54339.

--- frontend/aws_provision.sh
+++ frontend/aws_provision.sh
@@ -1,9 +1,9 @@
-aws ec2 run-instances --profile fe --image-id ami-0557a15b87f6559cf --count 1 --instance-type t2.micro \
+aws ec2 run-instances --image-id ami-0557a15b87f6559cf --count 1 --instance-type t2.micro \
     --key-name cra_ssh_key --security-groups cra_security_group \
     --tag-specifications \
         "ResourceType=instance,Tags=[{Key=Name,Value=cra_frontend}]" \
         "ResourceType=volume,Tags=[{Key=Name,Value=cra_frontend_disk1}]"
 
-aws ec2 describe-instances --profile fe --filters 'Name=tag:Name,Values=cra_frontend' 'Name=instance-state-name, Values=running' \
+aws ec2 describe-instances --filters 'Name=tag:Name,Values=cra_frontend' 'Name=instance-state-name, Values=running' \
     --query 'Reservations[*].Instances[*].[PublicIpAddress]' \
     --output text > ip.txt
