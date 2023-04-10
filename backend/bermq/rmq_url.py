786a09f2001a3bdcc4e78bbe3c3c31a4827e0bfc
morganhah99 <mjh58@njit.edu>
2023-04-10 13:27:53 -0400

    added endpoints

--- backend/bermq/rmq_url.py
+++ backend/bermq/rmq_url.py
@@ -1,1 +1,1 @@
-def rmq_url(): return 'amqps://b-c69444ee-13ed-43bb-b57c-3d5ad0cf8e2a.mq.us-east-1.amazonaws.com:5671'
+def rmq_url(): return 'amqps://b-03ace4a5-c513-496d-9d24-b0b0acd87f3c.mq.us-east-1.amazonaws.com:5671'
