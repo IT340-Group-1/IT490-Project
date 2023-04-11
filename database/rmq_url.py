6af3189fc64013a1dd67cfc33d51e84ee7113cac
morganhah99 <mjh58@njit.edu>
2023-04-11 16:38:20 -0400

    added endpoints

--- database/rmq_url.py
+++ database/rmq_url.py
@@ -1,1 +1,1 @@
-def rmq_url(): return 'amqps://b-03ace4a5-c513-496d-9d24-b0b0acd87f3c.mq.us-east-1.amazonaws.com:5671'
+def rmq_url(): return 'amqps://b-989fd41e-66cd-4dc3-bc1c-8e249d29a9bb.mq.us-east-1.amazonaws.com:5671'
